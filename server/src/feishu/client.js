9d68be6ceba9595c92582c354d4c6dd0433855d6
kimliang <l13026869463@gmail.com>
2026-06-03 11:42:53 +0800

    clean: 移除源码文件

--- server/src/feishu/client.js
+++
@@ -1,9 +0,0 @@
-const lark = require('@larksuiteoapi/node-sdk')
-
-const client = new lark.Client({
-  appId: process.env.FEISHU_APP_ID,
-  appSecret: process.env.FEISHU_APP_SECRET,
-  domain: lark.Domain.Feishu,
-})
-
-module.exports = client
